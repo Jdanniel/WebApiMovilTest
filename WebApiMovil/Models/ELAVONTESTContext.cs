22296c9f83985ef5e2bd3940bddc9fa639517436
Daniel Ojeda <ing.danniel@outlook.com>
2020-05-06 22:24:47 -0500

    Se cambian conexiones a produccion

--- WebApiMovil/Models/ELAVONTESTContext.cs
+++ WebApiMovil/Models/ELAVONTESTContext.cs
@@ -905,8 +905,8 @@ namespace WebApiMovil.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                //optionsBuilder.UseSqlServer("Server=192.168.100.56;Persist Security Info=True;connect timeout=400000;Database=ELAVON;User Id=sa;Password=b4ckl45h;");
-                optionsBuilder.UseSqlServer("Server=192.168.5.68;Persist Security Info=True;connect timeout=400000;Database=ELAVON_PRODUCTIVO;User Id=sa;Password=micr0f0rmas;");
+                optionsBuilder.UseSqlServer("Server=192.168.100.56;Persist Security Info=True;connect timeout=400000;Database=ELAVON;User Id=sa;Password=b4ckl45h;");
+                //optionsBuilder.UseSqlServer("Server=192.168.5.68;Persist Security Info=True;connect timeout=400000;Database=ELAVON_PRODUCTIVO;User Id=sa;Password=micr0f0rmas;");
             }
         }
 
